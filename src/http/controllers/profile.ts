01c69fec8254e76257947b0db7e8d67c7a51984d
Henrique Pereira <henrique802011@gmail.com>
2023-10-12 15:58:01 -0300

    feat: create get profile controller and middleware of verify JWT

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,13 +1,18 @@
+import { makeGetUserProfileUseCase } from "@/use-cases/factories/make-get-user-profile-use-case";
 import { FastifyRequest, FastifyReply } from "fastify";
 
-export async function profile(
-  request: FastifyRequest,
-  reply: FastifyReply
-) {
-  
-  await request.jwtVerify()
+export async function profile(request: FastifyRequest, reply: FastifyReply) {
+  await request.jwtVerify();
 
-  console.log(request.user.sub)
+  const getUserProfile = makeGetUserProfileUseCase();
 
-  return reply.status(200).send();
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  });
+
+   Reflect.deleteProperty(user, 'password_hash')
+
+   return reply.status(200).send({
+    ...user
+   })
 }
