33a463766d0f0f049d1ed037cafc065b968e5502
Henrique Pereira <henrique802011@gmail.com>
2023-10-28 18:03:47 -0300

    feat: create controllers to check-ins route

---
+++ src/http/controllers/check-ins/metrics.ts
@@ -0,0 +1,15 @@
+import { makeGetUserMetricsUseCase } from "@/use-cases/factories/make-get-user-metrics-use-case";
+import { FastifyRequest, FastifyReply } from "fastify";
+import { z } from "zod";
+
+export async function metrics(request: FastifyRequest, reply: FastifyReply) {
+  const fetchUserCheckInsHistoryUseCase = makeGetUserMetricsUseCase();
+
+  const { checkInsCounter } = await fetchUserCheckInsHistoryUseCase.execute({
+    userId: request.user.sub,
+  });
+
+  return reply.status(201).send({
+    checkInsCounter,
+  });
+}
